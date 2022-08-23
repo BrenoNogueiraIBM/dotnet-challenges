fde6a58815502ef796d8a47986b847755a0042d9
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-23 15:17:15 -0300

    Desafio 13

---
+++ Desafio13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listX = new List<int>();
+            List<int> listY = new List<int>();
+            while (true)
+            {
+
+                Console.WriteLine("Digite coordenadas (x,y): ");
+                string[] values = Console.ReadLine().Trim().Split(" ");
+                int x = int.Parse(values[0]);
+                int y = int.Parse(values[1]);
+
+                if (x == 0 || y == 0)
+                    break;
+
+                listX.Add(x);
+                listY.Add(y);
+            }
+            Console.WriteLine();
+            QuadrantFinder qf = new QuadrantFinder();
+            foreach (var item in listX.Zip(listY, Tuple.Create))
+            {
+                Console.WriteLine(qf.FindQuadrant(item.Item1, item.Item2));
+            }
+
+        }
+    }
+}
---
+++ Desafio13/QuadrantFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio13
+{
+    class QuadrantFinder
+    {
+        public QuadrantFinder()
+        {
+        }
+
+        public string FindQuadrant(int x, int y)
+        {
+            if (x > 0 && y > 0)
+                return "primeiro";
+            else if (x < 0 && y < 0)
+                return "terceiro";
+            else if (x < 0 && y > 0)
+                return "segundo";
+            else
+                return "quarto";
+        }
+
+    }
+}

0fbfa87b97c31a9146b9322f8b79996ed32175f6
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-24 15:22:02 -0300

    Desafio 14

---
+++ Desafio14/ConsecutiveOddCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Desafio14
+{
+    public class ConsecutiveOddCalculator
+    {
+        public ConsecutiveOddCalculator()
+        {
+        }
+
+        public int RangeSum(int upperLimit, int lowerLimit)
+        {
+            int sum = 0;
+            for (int i = lowerLimit + 1; i < upperLimit; i++)
+                sum += i % 2 != 0 ? i : 0; 
+
+            return sum;
+        }
+    }
+}
---
+++ Desafio14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+
+            do
+            {
+                Console.Write("Digite o maior numero: ");
+                int upperLimit = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o menor numero: ");
+                int lowerLimit = int.Parse(Console.ReadLine());
+
+                ConsecutiveOddCalculator coc = new ConsecutiveOddCalculator();
+                Console.WriteLine("Soma = " + coc.RangeSum(upperLimit, lowerLimit));
+            } while (debug);
+        }
+    }
+}

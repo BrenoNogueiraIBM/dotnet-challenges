6fb3ce0d04930c8dc0ca4477deb9d19324c703e7
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-26 10:25:22 -0300

    Desafio 15

---
+++ Desafio15/FroggyGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio15
+{
+    class FroggyGame
+    {
+        public int Jump { get; set; }
+        public List<int> Pipes { get; set; }
+
+        public FroggyGame(int jump, List<int> pipes)
+        {
+            Jump = jump;
+            Pipes = pipes;
+        }
+
+        public string Play()
+        {
+            int currentHeight = Pipes.First();
+            foreach (var pipeHeight in Pipes)
+            {
+                if (!ValidateJump(currentHeight, pipeHeight))
+                    return "Game Over!";
+
+                currentHeight = pipeHeight;
+            }
+
+            return "You Win!";
+        }
+
+        private bool ValidateJump(int current, int pipe)
+        {
+            int delta = Math.Abs(pipe - current);
+            return delta >= 0 && delta <= Jump;
+        }
+    }
+}
---
+++ Desafio15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+
+            do
+            {
+                Console.WriteLine("Altura do salto do Froggy: ");
+                int jump = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Numero de Canos: ");
+                int nPipes = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Altura dos Canos (separar por espaços): ");
+                string[] values = Console.ReadLine().Trim().Split(" ");
+
+                List<int> pipes = new List<int>();
+                for (int i = 0; i < nPipes; i++)
+                    pipes.Add(int.Parse(values[i]));
+
+                FroggyGame fg = new FroggyGame(jump, pipes);
+                Console.WriteLine();
+                Console.WriteLine(fg.Play());
+                Console.WriteLine();
+            } while (debug);
+        }
+    }
+}

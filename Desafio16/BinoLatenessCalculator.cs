72f806ec9018270f891e0c0e0612f4e262f0ce55
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-26 14:44:02 -0300

    Desafio 16

---
+++ Desafio16/BinoLatenessCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace Desafio16
+{
+    internal class BinoLatenessCalculator
+    {
+        public BinoLatenessCalculator()
+        {
+        }
+
+        public int ToMinutes(int hours, int minutes)
+        {
+            return (hours * 60) + minutes;
+        }
+
+        public int BinoCommute(int minutes)
+        {
+            return minutes + 60;
+        }
+
+        public int BinoLateness(int hours, int minutes)
+        {
+            int meetTime = ToMinutes(8, 0);
+            int lateness = BinoCommute(ToMinutes(hours, minutes)) - meetTime; 
+
+            return lateness < 0 ? 0 : lateness;
+        }
+    }
+}
---
+++ Desafio16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+            do
+            {
+                List<string> times = new List<string>();
+                while (true)
+                {
+                    Console.Write("Digite o horário que Bino acordou (0 para parar): ");
+                    string value = Console.ReadLine().Trim();
+
+                    if (value == "0")
+                        break;
+
+                    times.Add(value);
+                }
+                Console.WriteLine();
+
+                BinoLatenessCalculator blc = new BinoLatenessCalculator();
+                foreach (var item in times)
+                {
+                    int hours = int.Parse(item.Split(":")[0]);
+                    int minutes = int.Parse(item.Split(":")[1]);
+                    Console.WriteLine("Atraso Maximo = " + blc.BinoLateness(hours, minutes));
+                }
+            } while (debug);
+        }
+    }
+}

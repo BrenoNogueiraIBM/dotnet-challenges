b1ed1cb0548646435e3db627b1a4c0d5ad704d62
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-09-05 10:18:56 -0300

    Desafio 21

---
+++ Desafio21/CharacterCounter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio21
+{
+    class CharacterCounter
+    {
+        public string BiggestWord { get; set; }
+        public CharacterCounter()
+        {
+            BiggestWord = "-";
+        }
+
+        private string SplitCheck(string target)
+        {
+            string[] values = target.Split(" ");
+            string lenghts = "";
+            foreach (string go in values)
+            {
+                if (go.Length >= BiggestWord.Length)
+                    BiggestWord = go;
+
+                lenghts += go.Length + "-";
+            }
+
+            return lenghts.Remove(lenghts.LastIndexOf("-"), 1);
+        }
+
+        public IEnumerable<string> CountCharacters(List<string> list)
+        {
+            List<string> lenghts = new List<string>();
+            foreach (var item in list)
+            {
+                lenghts.Add(SplitCheck(item));
+            }
+
+            return lenghts;
+        }
+    }
+}
---
+++ Desafio21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mande suas mensagens!");
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string value = Console.ReadLine().Trim();
+
+                if (value == "0")
+                    break;
+
+                list.Add(value);
+            }
+
+            CharacterCounter cc = new CharacterCounter();
+            Console.WriteLine("\n-----------------------------------------------\n");
+            foreach (var item in cc.CountCharacters(list))
+                Console.WriteLine(item);
+            
+            Console.WriteLine("Maior palavra: " + cc.BiggestWord);
+        }
+    }
+}
